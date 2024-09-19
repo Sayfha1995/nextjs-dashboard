68d3adbf6b3219f44e0e316b06157b2be53c1d21
Sayfha1995 <siripaiboon.it64@gmail.com>
2024-09-19 09:38:17 +0700

    .

---
+++ app/ui/hero-image.tsx
@@ -0,0 +1,22 @@
+export function MobileHero() {
+  return (
+    <img 
+    src="/hero-mobile.png" 
+    alt="Screenshot of the dashboard project showing mobile version" 
+    width={560}
+    height={620}
+    />
+  );
+}
+
+export function DesktopHero() {
+  return (
+    <img 
+    src="/hero-desktop.png" 
+    alt="Screenshots of the dashboard project showing desktop version"
+    width={1000}
+    height={760}
+    />
+  );
+}
+
